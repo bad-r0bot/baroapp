1eb1419da0af6a5065f3759a13fd1a37fe64641c
Jim <hellodownthere360@gmail.com>
2016-02-10 13:48:52 +0100

    Buttons
    Linked all buttons to activities.

--- app/src/main/java/com/baromet/j/baroapp/Overzichtscherm.java
+++ app/src/main/java/com/baromet/j/baroapp/Overzichtscherm.java
@@ -1,13 +1,44 @@
 package com.baromet.j.baroapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Overzichtscherm extends AppCompatActivity {
+public class Overzichtscherm extends AppCompatActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overzichtscherm);
+    }
+
+    public void clickHoofdscherm(View v)
+    {
+        //Go to hoofdscherm
+        Button button13=(Button) v;
+        startActivity(new Intent(Overzichtscherm.this, Hoofdscherm.class));
+    }
+
+    public void clickInvoerscherm(View v)
+    {
+        //Go to invoerscherm
+        Button button14=(Button) v;
+        startActivity(new Intent(Overzichtscherm.this, Invoerscherm.class));
+    }
+
+    public void clickOverzichtscherm(View v)
+    {
+        //Go to overzichtscherm
+        Button button15=(Button) v;
+        startActivity(new Intent(Overzichtscherm.this, Overzichtscherm.class));
+    }
+
+    public void clickVakdetailscherm(View v)
+    {
+        //Go to vakdetailscherm
+        Button button16=(Button) v;
+        startActivity(new Intent(Overzichtscherm.this, Vakdetailscherm.class));
     }
 }
--- app/src/main/java/com/baromet/j/baroapp/Vakdetailscherm.java
+++ app/src/main/java/com/baromet/j/baroapp/Vakdetailscherm.java
@@ -1,13 +1,44 @@
 package com.baromet.j.baroapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Vakdetailscherm extends AppCompatActivity {
+public class Vakdetailscherm extends AppCompatActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vakdetailscherm);
+    }
+
+    public void clickHoofdscherm(View v)
+    {
+        //Go to hoofdscherm
+        Button button9=(Button) v;
+        startActivity(new Intent(Vakdetailscherm.this, Hoofdscherm.class));
+    }
+
+    public void clickInvoerscherm(View v)
+    {
+        //Go to invoerscherm
+        Button button10=(Button) v;
+        startActivity(new Intent(Vakdetailscherm.this, Invoerscherm.class));
+    }
+
+    public void clickOverzichtscherm(View v)
+    {
+        //Go to overzichtscherm
+            Button button11=(Button) v;
+        startActivity(new Intent(Vakdetailscherm.this, Overzichtscherm.class));
+    }
+
+    public void clickVakdetailscherm(View v)
+    {
+        //Go to vakdetailscherm
+        Button button12=(Button) v;
+        startActivity(new Intent(Vakdetailscherm.this, Vakdetailscherm.class));
     }
 }
