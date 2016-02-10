cacf856af1869ea2ae3f5fe689236e926511893b
Jim <hellodownthere360@gmail.com>
2016-02-10 12:47:59 +0100

    Linked buttons to activity.

--- app/src/main/java/com/baromet/j/baroapp/Hoofdscherm.java
+++ app/src/main/java/com/baromet/j/baroapp/Hoofdscherm.java
@@ -1,5 +1,6 @@
 package com.baromet.j.baroapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,22 +18,30 @@ public class Hoofdscherm extends AppCompatActivity implements OnClickListener {
 
     public void clickHoofdscherm(View v)
     {
+        //Go to hoofdscherm
         Button button=(Button) v;
+        startActivity(new Intent(Hoofdscherm.this, Hoofdscherm.class));
     }
 
     public void clickInvoerscherm(View v)
     {
+        //Go to invoerscherm
         Button button2=(Button) v;
+        startActivity(new Intent(Hoofdscherm.this, Invoerscherm.class));
     }
 
     public void clickOverzichtscherm(View v)
     {
+        //Go to overzichtscherm
         Button button3=(Button) v;
+        startActivity(new Intent(Hoofdscherm.this, Overzichtscherm.class));
     }
 
     public void clickVakdetailscherm(View v)
     {
+        //Go to vakdetailscherm
         Button button4=(Button) v;
+        startActivity(new Intent(Hoofdscherm.this, Vakdetailscherm.class));
     }
 
 }
