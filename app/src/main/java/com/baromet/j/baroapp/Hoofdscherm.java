5314a81a590ddc9b73603c9548fa1d4bc077f94b
Jim <hellodownthere360@gmail.com>
2016-02-10 12:42:54 +0100

    Added buttons
    4 buttons clickable.

--- app/src/main/java/com/baromet/j/baroapp/Hoofdscherm.java
+++ app/src/main/java/com/baromet/j/baroapp/Hoofdscherm.java
@@ -2,8 +2,10 @@ package com.baromet.j.baroapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Hoofdscherm extends AppCompatActivity {
+public class Hoofdscherm extends AppCompatActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,4 +14,25 @@ public class Hoofdscherm extends AppCompatActivity {
 
         //
     }
+
+    public void clickHoofdscherm(View v)
+    {
+        Button button=(Button) v;
+    }
+
+    public void clickInvoerscherm(View v)
+    {
+        Button button2=(Button) v;
+    }
+
+    public void clickOverzichtscherm(View v)
+    {
+        Button button3=(Button) v;
+    }
+
+    public void clickVakdetailscherm(View v)
+    {
+        Button button4=(Button) v;
+    }
+
 }
---
+++ app/src/main/java/com/baromet/j/baroapp/OnClickListener.java
@@ -0,0 +1,7 @@
+package com.baromet.j.baroapp;
+
+/**
+ * Created by Obb on 10/02/2016.
+ */
+public interface OnClickListener {
+}
