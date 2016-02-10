64da0385a0a6cc90884c1c0ba6e4a30c153cb621
Jim <hellodownthere360@gmail.com>
2016-02-10 17:09:56 +0100

    Attempting asynctask

--- app/src/main/java/com/baromet/j/baroapp/Overzichtscherm.java
+++ app/src/main/java/com/baromet/j/baroapp/Overzichtscherm.java
@@ -1,6 +1,7 @@
 package com.baromet.j.baroapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,10 +10,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -31,12 +41,12 @@ public class Overzichtscherm extends AppCompatActivity implements OnClickListene
     private ListView list;
     private String urljson;
 
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overzichtscherm);
-
-        new UpdateTask().execute();
 
         urljson = "http://www.fuujokan.nl/subject_list.json";
 
@@ -44,35 +54,33 @@ public class Overzichtscherm extends AppCompatActivity implements OnClickListene
 
         vakkenlijst(list);
         Log.d("oncreate", "vakkenlijst is creating");
+
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void clickHoofdscherm(View v)
-    {
+    public void clickHoofdscherm(View v) {
         //Go to hoofdscherm
-        Button button13=(Button) v;
+        Button button13 = (Button) v;
         startActivity(new Intent(Overzichtscherm.this, Hoofdscherm.class));
     }
 
-    public void clickInvoerscherm(View v)
-    {
+    public void clickInvoerscherm(View v) {
         //Go to invoerscherm
-        Button button14=(Button) v;
+        Button button14 = (Button) v;
         startActivity(new Intent(Overzichtscherm.this, Invoerscherm.class));
     }
 
-    public void clickOverzichtscherm(View v)
-    {
+    public void clickOverzichtscherm(View v) {
         //Go to overzichtscherm
-        Button button15=(Button) v;
+        Button button15 = (Button) v;
         startActivity(new Intent(Overzichtscherm.this, Overzichtscherm.class));
 
 
     }
 
-    public void clickVakdetailscherm(View v)
-    {
+    public void clickVakdetailscherm(View v) {
         //Go to vakdetailscherm
-        Button button16=(Button) v;
+        Button button16 = (Button) v;
         startActivity(new Intent(Overzichtscherm.this, Vakdetailscherm.class));
     }
 
@@ -85,35 +93,68 @@ public class Overzichtscherm extends AppCompatActivity implements OnClickListene
         return sb.toString();
     }
 
-    private class UpdateTask extends AsyncTask<String, String,String> {
-        protected String doInBackground(String urljson) {
+    @Override
+    public void onStart() {
+        super.onStart();
 
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost(urljson);
-
-            HttpResponse httpResponse = null;
-            try {
-                httpResponse = httpClient.execute(httpPost);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            HttpEntity httpEntity = httpResponse.getEntity();
-            try {
-                InputStream is = httpEntity.getContent();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            return null;
-        }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Overzichtscherm Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.baromet.j.baroapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
     }
 
-    private JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-        InputStream is = new URL(url).openStream();
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Overzichtscherm Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.baromet.j.baroapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    private void makeGetRequest() {
+
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet(urljson);
+
+        HttpResponse response;
+        try {
+            response = client.execute(request);
+
+            Log.d("Response of GET request", response.toString());
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    private JSONObject readJsonFromUrl(String urljson) throws IOException, JSONException {
+        InputStream is = new URL(urljson).openStream();
         try {
             BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
             String jsonText = readAll(rd);
@@ -124,7 +165,7 @@ public class Overzichtscherm extends AppCompatActivity implements OnClickListene
         }
     }
 
-    private void vakkenlijst (View v){
+    private void vakkenlijst(View v) {
         try {
             list.setAdapter(new JsonListAdapter(readJsonFromUrl(urljson)));
             Log.d("setAdapter", "Setting adapter through urljson");
