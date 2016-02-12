72f3d2a9ef00fed9e1f12c83d3c2e3795aaedf0b
Jim <hellodownthere360@gmail.com>
2016-02-12 16:47:52 +0100

    Attempting Json again
    Trying to fix the whole asynctask and jsonparsing.

--- app/src/main/java/com/baromet/j/baroapp/Overzichtscherm.java
+++ app/src/main/java/com/baromet/j/baroapp/Overzichtscherm.java
@@ -3,8 +3,8 @@ package com.baromet.j.baroapp;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,17 +14,14 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HttpContext;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -40,10 +37,6 @@ public class Overzichtscherm extends AppCompatActivity implements OnClickListene
 
     private ListView list;
     private String urljson;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
     private GoogleApiClient client;
 
     @Override
@@ -52,14 +45,11 @@ public class Overzichtscherm extends AppCompatActivity implements OnClickListene
         setContentView(R.layout.activity_overzichtscherm);
 
 
-        urljson = "http://www.fuujokan.nl/subject_list.json";
+        urljson = "http://www.fuujokan.nl/subject_lijst.json";
 
 
         vakkenlijst(list);
         Log.d("oncreate", "vakkenlijst is creating");
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public void clickHoofdscherm(View v) {
@@ -97,125 +87,42 @@ public class Overzichtscherm extends AppCompatActivity implements OnClickListene
         return sb.toString();
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
+    URL requestUrl = "http://www.fuujokan.nl/subject_lijst.json";
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Overzichtscherm Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.baromet.j.baroapp/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
+    private void jsonTest(){
+    new AsyncTask<URL, Void, JSONObject>() {
 
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Overzichtscherm Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.baromet.j.baroapp/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
-
-    private void makeGetRequest() {
-
-        HttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet(urljson);
-        // replace with your url
-
-        HttpResponse response;
-        try {
-            response = client.execute(request);
-
-            Log.d("Response of GET request", response.toString());
-        } catch (ClientProtocolException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-    }
-
-    private JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-        InputStream is = null;
-
-        NetworkThread networkThread= new NetworkThread(is);
-        networkThread.execute();
-
-        if(is != null){
-
-        try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String jsonText = readAll(rd);
-            JSONObject json = new JSONObject(jsonText);
-            return json;
-        } finally {
-            is.close();
-        }}
-        else{
-            Log.d("Thread madness", "Thread didnt fill empty inputstream");
-        }
-        return null;
-    }
-
-    private void vakkenlijst(View v) {
-        try {
-
-            ListView list = (ListView) findViewById(R.id.vakkenlijst);
-            list.setAdapter(new JsonListAdapter(readJsonFromUrl(urljson)));
-            Log.d("setAdapter", "Setting adapter through urljson");
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.d("AAAAGH", "IO Exceptopn while loading list\n" + e.getMessage());
-        } catch (JSONException e) {
-            e.printStackTrace();
-
-            Log.d("AAAAGH", "JSONException while loading list\n" + e.getMessage());
-        }
-    }
-
-    class NetworkThread extends AsyncTask<String, Void, Boolean> {
-
-        private InputStream is = null;
-        public NetworkThread(InputStream is){
-            this.is = is;
-        }
-
-        public void run(){
-
+        @Override
+        protected Boolean doInBackground(URL requestUrl) {
+            loadJSON(url);
         }
 
         @Override
-        protected Boolean doInBackground(String... params) {
+        protected void onPostExecute(JSONObject jsonData) {
             try {
-                is = new URL(urljson).openStream();
-            } catch (IOException e) {
-                e.printStackTrace();
+                // Getting Array of albums
+
+                albums = json.getJSONArray(TAG_ALBUMS);
+                sngs=json.getJSONArray(TAG_SONGS);
+                // looping through All albums
+
+                etc.
             }
-            return null;
-        }
+        }.execute(requestUrl);
+
+
+    public void loadJSON(URL url) {
+        // Creating JSON Parser instance
+        JSONParser jParser = new JSONParser();
+
+        // getting JSON string from URL
+        JSONObject json = jParser.getJSONFromUrl(url);
+
+        return json;
     }
+
+}
+}
+
 
 }
